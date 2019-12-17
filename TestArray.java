29b5927f34da0c6f0dd55cb9be25adcf7185e351
宁川 <395378476@qq.com>
2019-12-17 21:12:49 +0800

    代码提交

---
+++ TestArray.java
@@ -0,0 +1,7 @@
+public class TestArray{
+	public static void main(String[] args){
+		int[] array = new int[5]{10,20,30,40,50};
+		array[0]= 100
+		System.out.println(array[0]);
+	}
+}
--- WhileStar.java
+++ WhileStar.java
@@ -1,13 +1,37 @@
+import java.util.Scanner;
+
 public class WhileStar{
 	public static void main(String[]args){
+		Scanner input = new Scanner(System.in);
+		System.out.println("您想要画几行星星呢？");
+		int line = input.nextInt();
 		int i = 1;
-		while(i<=4){
-			if(i=1){
-				System.out.print("*******");
+		while(i<=line){
+			if(i==1){
+				int x = 1;
+				while(x<=2*line-1){
+					System.out.print("*");
+					x++;
+				}
+			}else{
+				int j = 1;
+				while(j<=line+1-i){
+					System.out.print("*");
+					j++;
+				}
+				int k = 1;
+				while(k<=2*i-3){
+					System.out.print(" ");
+					k++;
+				}
+				int l = 1;
+				while(l<=line+1-i){
+					System.out.print("*");
+					l++;
+				}
 			}
-			while(){
-			}
-			i++
+			System.out.println();
+			i++;
 		}
 	}
 }
