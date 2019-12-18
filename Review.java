95c1463070ea88a65684e6d6bc30652e9adb0c0f
宁川 <395378476@qq.com>
2019-12-18 23:33:26 +0800

    代码提交

---
+++ Review.java
@@ -0,0 +1,20 @@
+public class Review{
+	public static void main(String[] args){
+		//int a = 1000;
+		//byte b = (byte)a;输出的值是-24，是因为计算机存的是反码。
+		//int a = 1;
+		//float b = a;
+		//long a = 1;
+		//float b = a;
+		//char x = 'b';
+		//int y = x;
+		//long a = 2147489999L;
+		//float b = a;
+		//int a =40000;
+		//char b = (char)a;
+		byte x = 1;
+		x+=6;
+		byte y = x;
+		System.out.println(y);
+	}
+}
