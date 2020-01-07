5fee488bad88e201bba1dcc5a424f2be02b5d7ec
宁川 <395378476@qq.com>
2020-01-07 23:49:22 +0800

    代码提交

--- Test0101.java
+++ Test0101.java
@@ -1,12 +1,13 @@
 public class Test0101 {
     public static void main(String[] args){
         int[] array = {1,2,3,4,5,6,7,8,};
-        binarySearch(array,9);
+        String result = binarySearch(array,6);
+        System.out.println(result);
     }
-    public static void binarySearch( int[] x, int num){
+    public static String binarySearch( int[] x, int num){
         int left = 0;
         int right = x.length-1;
-        boolean a = true;
+        String result = "您要找的元素在数组中不存在";
         while(left<=right){
             int mid = (left+right)/2;
             if(num<x[mid]){
@@ -14,13 +15,10 @@ public class Test0101 {
             }else if(num>x[mid]){
                 left = mid+1;
             }if(num==x[mid]){
-                System.out.println("您要找的元素在数组中的索引为"+mid);
-                a = false;
+                result = "您要找的元素在数组中的索引为"+mid;
                 break;
             }
         }
-        if(a){
-            System.out.println("您要找的元素在数组中不存在");
-        }
+        return result;
     }
 }
