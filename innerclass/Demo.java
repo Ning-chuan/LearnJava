5d05804c8be01b8586edb05fab58f2c4488587c2
宁川 <395378476@qq.com>
2020-01-19 23:51:51 +0800

    代码提交

---
+++ innerclass/Demo.java
@@ -0,0 +1,33 @@
+package innerclass;
+
+public class Demo {
+    private String name = "这是正常类中的属性";
+    public void testDemo(){
+        System.out.println("这是正常类中的方法");
+    }
+    //成员内部类
+    public class InnerDemo{
+        private String name="我是内部类的属性";
+        public void testInnerDemo(){
+            System.out.println("我是成员内部类的方法:"+this.name);
+            Demo.this.testDemo();
+        }
+    }
+    public static class InnerDemoStatic{
+
+    }
+    public void testDemoOne(){
+        String s = "";
+        //定义一个局部内部类
+        class InnerTestMethod{
+            //局部内部类中使用的局部变量都需要加final修饰
+        }
+    }
+    public void testDemoTwo(){
+        String s;
+        //定义一个局部内部类
+        class InnerTestMethod{
+
+        }
+    }
+}
