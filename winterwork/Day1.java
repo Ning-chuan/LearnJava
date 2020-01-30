a06d78c62cb9b3a2469847f1a1763fac3e03cd44
宁川 <395378476@qq.com>
2020-01-30 21:37:38 +0800

    代码提交

--- winterwork/Day1.java
+++ winterwork/Day1.java
@@ -17,20 +17,40 @@ public class Day1 {
     //5 给定一个数组，将数组中的元素向右移动k个位置，其中k是非负数。
     //输入: [1,2,3,4,5,6,7] 和 k = 3
     //输出: [5,6,7,1,2,3,4]
+    public void reverse(int[] arr,int start,int end){
+        while(start<end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
     public void rotate(int[] nums, int k) {
+        //方法三：三次反转
         k%=nums.length;
-        int index = 0;
-        int[] newNums = new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            if(i<k){
-                newNums[i] = nums[nums.length-k+i];
-            }else{
-                newNums[i] = nums[index++];
-            }
-        }
-        for(int i=0;i<nums.length;i++){
-            nums[i] = newNums[i];
-        }
+        reverse(nums,0,nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,nums.length-1);
+
+        //方法一：原本数组里下标为i的放到 (i+k)%数组长度 的位置
+//        int[] newNums = new int[nums.length];
+//        for(int i=0;i<nums.length;i++){
+//            newNums[(i+k)%nums.length] = nums[i];
+//        }
+//        for(int i=0;i<nums.length;i++){
+//            nums[i] = newNums[i];
+//        }
+
+        //方法二：
+//        k%=nums.length;
+//        for(int i=0;i<k;i++){//旋转k次
+//            for(int j=0;j<nums.length;j++){//旋转一个元素
+//                int temp = nums[j];
+//                nums[j] = nums[nums.length-1];
+//                nums[nums.length-1] = temp;
+//            }
+//        }
     }
 
     //===================================================================
@@ -41,7 +61,7 @@ public class Day1 {
 //        System.out.println(result);
 
         int[] nums = new int[]{1,2,5,6,8,9};
-        test.rotate(nums,3);
+        test.rotate(nums,8);
         for(int v:nums){
             System.out.println(v);
         }
