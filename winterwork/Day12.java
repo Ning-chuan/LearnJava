ed1292255364eaa4465c70f059c3f30fc8b05679
宁川 <395378476@qq.com>
2020-02-21 22:47:54 +0800

    代码提交

--- winterwork/Day12.java
+++ winterwork/Day12.java
@@ -1,5 +1,17 @@
 package winterwork;
 
 public class Day12 {
-
+    //4 最短无序连续子数组
+        //https://leetcode-cn.com/problems/shortest-unsorted-continuous-subarray/
+        public int findUnsortedSubarray(int[] nums) {
+            int start = 0;
+            int end = nums.length-1;
+            while(nums[start]<nums[start+1]){
+                start++;
+            }
+            while(nums[end] > nums[end-1]){
+                end--;
+            }
+            return end-start+1;
+    }
 }
