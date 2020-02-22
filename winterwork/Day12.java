30b5dd5e65d561bd66ce43b7007e905b5347a983
宁川 <395378476@qq.com>
2020-02-22 21:44:26 +0800

    代码提交

--- winterwork/Day12.java
+++ winterwork/Day12.java
@@ -4,14 +4,39 @@ public class Day12 {
     //4 最短无序连续子数组
         //https://leetcode-cn.com/problems/shortest-unsorted-continuous-subarray/
         public int findUnsortedSubarray(int[] nums) {
-            int start = 0;
-            int end = nums.length-1;
-            while(nums[start]<nums[start+1]){
-                start++;
+            int max = nums[0];
+            int min = nums[nums.length-1];
+            int left = 0, right = -1;
+            for(int i=0;i<nums.length;i++){
+                if(max>nums[i]){
+                    right = i;
+                }else{
+                    max = nums[i];
+                }
+                if(min<nums[nums.length-i-1]){
+                    left = nums.length-i-1;
+                }else{
+                    min = nums[nums.length-i-1];
+                }
             }
-            while(nums[end] > nums[end-1]){
-                end--;
-            }
-            return end-start+1;
+            return right-left+1;
+    }
+
+    //5 逆波兰表达式求值
+    //根据逆波兰表示法，求表达式的值。
+    //有效的运算符包括 +, -, *, / 。每个运算对象可以是整数，也可以是另一个逆波兰表达式。
+    public int evalRPN(String[] tokens) {
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
