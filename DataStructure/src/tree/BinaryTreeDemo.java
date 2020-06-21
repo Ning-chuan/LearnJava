a43c82edffe0d0a7557beb9ed613f1487d8cc1cd
Ning-chuan <395378476@qq.com>
2020-06-21 10:22:38 +0800

    二叉树的前序、中序、后续查找、删除节点的代码实现

--- DataStructure/src/tree/BinaryTreeDemo.java
+++ DataStructure/src/tree/BinaryTreeDemo.java
@@ -16,13 +16,33 @@ public class BinaryTreeDemo {
         //创建二叉树并设置根节点
         HeroBinaryTree tree = new HeroBinaryTree();
         tree.setRoot(hero1);
-        //测试三种遍历
-        System.out.println("前序遍历结果：");
+
+//        //测试三种遍历
+//        System.out.println("前序遍历结果：");
+//        tree.preOrder();
+//        System.out.println("中序遍历结果：");
+//        tree.infixOrder();
+//        System.out.println("后序遍历结果：");
+//        tree.postOrder();
+//
+//        System.out.println("=================================");
+//        HeroNode res1 = tree.preOrderSearch(5);
+//        System.out.println("前序查找结果为"+res1);
+//        System.out.println("=================================");
+//        HeroNode res2 = tree.infixOrderSearch(5);
+//        System.out.println("中序查找结果为"+res2);
+//        System.out.println("=================================");
+//        HeroNode res3 = tree.postOrderSearch(5);
+//        System.out.println("后序查找结果为"+res3);
+//        System.out.println("=================================");
+
+        //测试删除节点
+        System.out.println("删除前前序遍历");
         tree.preOrder();
-        System.out.println("中序遍历结果：");
-        tree.infixOrder();
-        System.out.println("后序遍历结果：");
-        tree.postOrder();
+        tree.delNode(13);
+        System.out.println("删除后前序遍历");
+        tree.preOrder();
+
     }
 
 }
@@ -33,6 +53,8 @@ class HeroBinaryTree{
     public void setRoot(HeroNode root){
         this.root = root;
     }
+
+    //===========================================================================
     //前序遍历 （先输出，再左、右）
     public void preOrder(){
         if(this.root != null){
@@ -95,6 +117,140 @@ class HeroBinaryTree{
         }
         //3.输出父节点
         System.out.println(heroNode);
+    }
+
+    //=============================================================================
+
+    //前序查找
+    public HeroNode preOrderSearch(int no){
+        if(this.root != null){
+            return preOrderSearch(root, no);
+        }
+        return null;
+    }
+    //供自己递归调用的前序查找方法
+    private HeroNode preOrderSearch(HeroNode heroNode,int no){
+        //1.和当前节点no比较
+        System.out.println("前序查找即将进行比较~");//测试比较次数的语句
+        if(heroNode.getNo() == no){
+            return heroNode;
+        }
+        //2.如果no不相等，向左递归前序查找
+        HeroNode res = null;
+        if(heroNode.getLeft() != null){
+            res = preOrderSearch(heroNode.getLeft(), no);
+        }
+        if(res != null){
+            return res;
+        }
+        //3.如果左递归没有找到，向右递归前序查找
+        if(heroNode.getRight() != null){
+            res = preOrderSearch(heroNode.getRight(), no);
+        }
+        return res;
+    }
+
+    //中序查找
+    public HeroNode infixOrderSearch(int no){
+        if(this.root != null){
+            return infixOrderSearch(root, no);
+        }
+        return null;
+    }
+    //供自己递归调用的中序查找方法
+    private HeroNode infixOrderSearch(HeroNode heroNode,int no){
+        //左递归中序查找
+        HeroNode res = null;
+        if(heroNode.getLeft() != null){
+            res = infixOrderSearch(heroNode.getLeft(), no);
+        }
+        if(res != null){
+            return res;
+        }
+        //左递归没有找到，和当前节点no比较
+        System.out.println("中序查找即将进行比较~");//测试比较次数的语句
+        if(heroNode.getNo() == no){
+            return heroNode;
+        }
+        //no不相等，右递归中序查找
+        if(heroNode.getRight() != null){
+            res = infixOrderSearch(heroNode.getRight(), no);
+        }
+        return res;
+    }
+
+    //后序查找
+    public HeroNode postOrderSearch(int no){
+        if(this.root != null){
+            return postOrderSearch(root, no);
+        }
+        return null;
+    }
+    //供自己递归调用的后序查找方法
+    private HeroNode postOrderSearch(HeroNode heroNode,int no){
+        //左递归后序查找
+        HeroNode res = null;
+        if(heroNode.getLeft() != null){
+            res = postOrderSearch(heroNode.getLeft(), no);
+        }
+        if(res != null){
+            return res;
+        }
+        //左递归没有找到，右递归后序查找
+        if(heroNode.getRight() != null){
+            res = postOrderSearch(heroNode.getRight(), no);
+        }
+        if(res != null){
+            return res;
+        }
+        //右递归没有找到，最后和当前节点no比较
+        System.out.println("后序查找即将进行比较~");//测试比较次数的语句
+        if(heroNode.getNo() == no){
+            return heroNode;
+        }
+        return null;
+    }
+    //=========================================================================
+
+    //删除节点 (规则：如果要删除的节点是叶子节点，直接删除；如果是非叶子节点，则删除该子树。)
+    public void delNode(int no){
+        if(root == null){
+            System.out.println("空树，删除不得");
+        }else if(root.getNo() == no){
+            root = null;
+        }else{
+            delNode(root,no);
+        }
+    }
+    /**
+     * @param heroNode 当前节点
+     * @param no 要删除的节点
+     * @return -1表示没有找到要删除的节点  1表示删除成功
+     */
+    private int delNode(HeroNode heroNode, int no) {
+        //1.判断当前节点左子节点是否是待删除节点，如果是，置为空
+        if(heroNode.getLeft() != null && heroNode.getLeft().getNo() == no){
+            heroNode.setLeft(null);
+            return 1;
+        }
+        //2.判断当前节点右子节点是否是待删除节点，如果是，置为空
+        if(heroNode.getRight() != null && heroNode.getRight().getNo() == no){
+            heroNode.setRight(null);
+            return 1;
+        }
+        //3.向左递归调用删除方法
+        int res = -1;//接收结果，左递归完毕后用该结果进行判断，初始值给-1
+        if(heroNode.getLeft() != null){
+            res = delNode(heroNode.getLeft(),no);
+        }
+        if(res == 1){
+            //说明删除成功了，就不执行后面的代码了
+            return res;
+        }
+        if(heroNode.getRight() != null){
+            res = delNode(heroNode.getRight(),no);
+        }
+        return res;
     }
 
 }
