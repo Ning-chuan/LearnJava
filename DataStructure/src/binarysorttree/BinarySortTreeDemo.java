a7c26c10a5c46f9a8a9250b31bec4cb3654b2d3d
Ning-chuan <395378476@qq.com>
2020-07-14 23:32:50 +0800

    二叉排序树的删除：要删除节点是叶子节点和只有一颗子树的情况

--- DataStructure/src/binarysorttree/BinarySortTreeDemo.java
+++ DataStructure/src/binarysorttree/BinarySortTreeDemo.java
@@ -7,6 +7,17 @@ public class BinarySortTreeDemo {
         for (int value : arr) {
             binarySortTree.addNode(new Node(value));
         }
+        System.out.println("删除节点前，二叉树为：");
+        binarySortTree.inOrder();
+
+        binarySortTree.delNode(2);
+        binarySortTree.delNode(9);
+        binarySortTree.delNode(12);
+        binarySortTree.delNode(5);
+        binarySortTree.delNode(10);
+        binarySortTree.delNode(90);
+
+        System.out.println("删除节点后，二叉树为：");
         binarySortTree.inOrder();
     }
 
@@ -15,6 +26,9 @@ public class BinarySortTreeDemo {
 class BinarySortTree{
     Node root;
 
+    public Node getRoot() {
+        return root;
+    }
     //添加节点的方法
     public void addNode(Node node){
         if(root == null){
@@ -62,6 +76,134 @@ class BinarySortTree{
         }
     }
 
+    //删除节点方法：
+    public void delNode(int value) {
+        Node target = search(value);
+        if(target == null){
+            System.out.println("没有找到要删除的节点");
+            return;
+        }
+        Node parent = searchParent(value);
+        if (target.left == null && target.right == null) {
+            //情况一：要删除的节点是叶子节点
+            if(parent != null){
+                if(parent.left == target){
+                    //该节点是父节点的左子节点
+                    parent.left = null;
+                }else{
+                    //该节点是父节点的右子节点
+                    parent.right = null;
+                }
+            }else{
+                //要删除的节点是根节点
+                root = null;
+            }
+        } else if (target.left != null && target.right != null) {
+            //情况二：要删除的节点右两个子树 找到该节点左子树值最大的节点 覆盖目标节点
+            if(parent != null){
+                findLeftMaxNode(target);
+            }else{
+                //要删除的节点是根节点
+
+            }
+        } else {
+            //情况三：要删除的节点只有一个子树
+            if(parent != null){
+                if(parent.left != null){
+                    //目标节点是父节点的左子树
+                    if(target.left != null){
+                        parent.left = target.left;
+                    }else{
+                        parent.left = target.right;
+                    }
+                }else{
+                    //目标节点是父节点的右子树
+                    if(target.left != null){
+                        parent.right = target.left;
+                    }else{
+                        parent.right = target.right;
+                    }
+                }
+            }else{
+                //要删除的节点是根节点
+                if(root.left != null){
+                    root = root.left;
+                }else{
+                    root = root.right;
+                }
+            }
+        }
+    }
+
+    //找到左子树值最大的节点
+    private void findLeftMaxNode(Node target) {
+
+    }
+
+    //方法：根据value找到目标节点
+    public Node search(int value){
+        if(root == null){
+            System.out.println("空树");
+            return null;
+        }else{
+            return search(root, value);
+        }
+    }
+    private Node search(Node node,int value){
+        //和当前节点比较
+        if(node.value == value){
+            return node;
+        }
+        //分两种情况处理
+        if(value < node.value){
+            //小于当前节点的值 比较左子节点
+            if (node.left != null) {
+                //向左递归查找
+                return search(node.left, value);
+            }else{
+                //左子节点为空 说明不存在
+                return null;
+            }
+        }else{
+            //大于当前节点的值 比较右子节点
+            if (node.right != null) {
+                //向右递归查找
+                return search(node.right, value);
+            }else{
+                //右子节点为空 说明不存在
+                return null;
+            }
+        }
+    }
+
+    //方法：寻找目标节点的父节点
+    public Node searchParent(int value){
+        if(root == null || root.value == value){
+            //根节点为空 或者根节点即为目标节点 这两种情况都不存在父节点 返回空
+            return null;
+        }else{
+            return searchParent(root, value);
+        }
+    }
+    private Node searchParent(Node node,int value){
+        if(node.left != null && node.left.value == value ||
+                node.right != null && node.right.value == value){
+            return node;
+        }
+        if(value < node.value){
+            if(node.left != null){
+                return searchParent(node.left, value);
+            }else{
+                return null;
+            }
+        }else{
+            if(node.right != null){
+                return searchParent(node.right, value);
+            }else{
+                return null;
+            }
+        }
+    }
 }
 class Node{
     int value;
