df8865d00ef21193de45fc2a623166da96bda381
Ning-chuan <395378476@qq.com>
2020-07-15 23:30:19 +0800

    二叉平衡树 求二叉树的高度

---
+++ DataStructure/src/avltree/AVLTreeDemo.java
@@ -0,0 +1,242 @@
+package tree.avltree;
+
+public class AVLTreeDemo {
+    public static void main(String[] args) {
+        int[] arr = {4, 3, 6, 5, 7, 8};
+        AVLTree tree = new AVLTree();
+        for (int value : arr) {
+            tree.addNode(new Node(value));
+        }
+
+        tree.inOrder();
+        System.out.println("AVL树根节点为：" + tree.getRoot());
+
+        System.out.println("当前树的高度为：" + tree.getHeight());
+    }
+}
+
+//以二叉排序为基础的二叉平衡树类
+class AVLTree {
+
+    Node root;
+
+    public Node getRoot() {
+        return root;
+    }
+
+    //方法：获取当前树的高度
+    public int getHeight() {
+        if (root == null) {
+            return 0;
+        }
+        return root.height();
+    }
+
+    //添加节点的方法
+    public void addNode(Node node) {
+        if (root == null) {
+            root = node;
+        } else {
+            addNode(root, node);
+        }
+    }
+
+    //自身递归调用的添加方法
+    private void addNode(Node temp, Node target) {
+        if (target == null) {
+            return;
+        }
+        if (target.value < temp.value) {
+            if (temp.left == null) {
+                temp.left = target;
+            } else {
+                addNode(temp.left, target);
+            }
+        } else {
+            //target值不小于temp 放在右边
+            if (temp.right == null) {
+                temp.right = target;
+            } else {
+                addNode(temp.right, target);
+            }
+        }
+    }
+
+    // 中序遍历方法
+    public void inOrder() {
+        if (this.root == null) {
+            System.out.println("空树，遍历个寂寞");
+        } else {
+            inOrder(root);
+        }
+    }
+
+    private void inOrder(Node node) {
+        if (node.left != null) {
+            inOrder(node.left);
+        }
+        System.out.println(node);
+        if (node.right != null) {
+            inOrder(node.right);
+        }
+    }
+
+    //删除节点方法：
+    public void delNode(int value) {
+        Node target = search(value);
+        if (target == null) {
+            System.out.println("没有找到要删除的节点");
+            return;
+        }
+        Node parent = searchParent(value);
+        if (target.left == null && target.right == null) {
+            //情况一：要删除的节点是叶子节点
+            if (parent != null) {
+                if (parent.left == target) {
+                    //该节点是父节点的左子节点
+                    parent.left = null;
+                } else {
+                    //该节点是父节点的右子节点
+                    parent.right = null;
+                }
+            } else {
+                //要删除的节点是根节点
+                root = null;
+            }
+        } else if (target.left != null && target.right != null) {
+            //情况二：要删除的节点右两个子树
+            //  找到目标节点左子树值最大的节点
+            Node maxLeftChildNode = this.findLeftMaxNode(target);
+            System.out.println("最大左子节点：" + maxLeftChildNode);
+            //  删除左子树最大节点
+            delNode(maxLeftChildNode.value);
+            //  用左子树最大节点的值覆盖目标节点
+            target.value = maxLeftChildNode.value;
+        } else {
+            //情况三：要删除的节点只有一个子树
+            if (parent != null) {
+                if (target.left != null) {
+                    //目标节点只有左子树
+                    if (parent.left == target) {
+                        parent.left = target.left;
+                    } else {
+                        parent.right = target.left;
+                    }
+                } else {
+                    //目标节点只有右子树
+                    if (parent.left == target) {
+                        parent.left = target.right;
+                    } else {
+                        parent.right = target.right;
+                    }
+                }
+            } else {
+                //要删除的节点是根节点
+                if (root.left != null) {
+                    root = root.left;
+                } else {
+                    root = root.right;
+                }
+            }
+        }
+    }
+
+    //找到左子树值最大的节点
+    private Node findLeftMaxNode(Node target) {
+        if (target.left == null) {
+            return null;
+        }
+        Node temp = target.left;
+        while (temp.right != null) {
+            temp = temp.right;
+        }
+        //循环结束 temp指向了左子树最大的节点
+        return temp;
+    }
+
+    //方法：根据value找到目标节点
+    public Node search(int value) {
+        if (root == null) {
+            System.out.println("空树");
+            return null;
+        } else {
+            return search(root, value);
+        }
+    }
+
+    private Node search(Node node, int value) {
+        //和当前节点比较
+        if (node.value == value) {
+            return node;
+        }
+        //分两种情况处理
+        if (value < node.value) {
+            //小于当前节点的值 比较左子节点
+            if (node.left != null) {
+                //向左递归查找
+                return search(node.left, value);
+            } else {
+                //左子节点为空 说明不存在
+                return null;
+            }
+        } else {
+            //大于当前节点的值 比较右子节点
+            if (node.right != null) {
+                //向右递归查找
+                return search(node.right, value);
+            } else {
+                //右子节点为空 说明不存在
+                return null;
+            }
+        }
+    }
+
+    //方法：寻找目标节点的父节点
+    public Node searchParent(int value) {
+        if (root == null || root.value == value) {
+            //根节点为空 或者根节点即为目标节点 这两种情况都不存在父节点 返回空
+            return null;
+        } else {
+            return searchParent(root, value);
+        }
+    }
+
+    private Node searchParent(Node node, int value) {
+        if (node.left != null && node.left.value == value ||
+                node.right != null && node.right.value == value) {
+            return node;
+        }
+        if (value < node.value) {
+            if (node.left != null) {
+                return searchParent(node.left, value);
+            } else {
+                return null;
+            }
+        } else {
+            if (node.right != null) {
+                return searchParent(node.right, value);
+            } else {
+                return null;
+            }
+        }
+    }
+}
+
+class Node {
+    int value;
+    Node left;
+    Node right;
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    public String toString() {
+        return "Node=[value=" + value + "]";
+    }
+
+    //方法：返回以当前节点为根节点的树的高度
+    public int height(){
+        return Math.max(left == null ? 0 : left.height(), right == null ? 0 : right.height()) + 1;
+    }
+}
