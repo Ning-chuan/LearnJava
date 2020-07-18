d123c1353b31d4130ec682e09204fb71477fb11f
Ning-chuan <395378476@qq.com>
2020-07-18 22:41:26 +0800

    springAOP学习

--- SpringAOP/src/aspect/Aspect1.java
+++ SpringAOP/src/aspect/Aspect1.java
@@ -4,4 +4,8 @@ public class Aspect1 {
     public void before(){
         System.out.println("Aspect1前置方法：");
     }
+
+    public void after(){
+        System.out.println("Aspect1最终通知");
+    }
 }
