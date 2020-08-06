bdd5fb7f50b303fe20bc14777c3e54c997b1b38a
Ning-chuan <395378476@qq.com>
2020-08-06 22:58:59 +0800

    nio实现网络通信

--- NIO/src/main/java/com/yuziyan/demo1/TestBlockingNIO.java
+++ NIO/src/main/java/com/yuziyan/demo1/TestBlockingNIO.java
@@ -2,11 +2,61 @@ package com.yuziyan.demo1;
 
 import org.junit.Test;
 
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 public class TestBlockingNIO {//没有使用Selector 阻塞式的
 
-    //
+    //客户端
     @Test
-    public void client(){
+    public void client() throws Exception{
+        SocketChannel sChannel=SocketChannel.open(new InetSocketAddress("127.0.0.1",9898));
+        FileChannel inChannel=FileChannel.open(Paths.get("src/1.jpg"), StandardOpenOption.READ);
+        ByteBuffer buf= ByteBuffer.allocate(1024);
+        while(inChannel.read(buf)!=-1){
+            buf.flip();
+            sChannel.write(buf);
+            buf.clear();
+        }
+        sChannel.shutdownOutput();//关闭发送通道，表明发送完毕
 
+        //接收服务端的反馈
+        int len=0;
+        while((len=sChannel.read(buf))!=-1){
+            buf.flip();
+            System.out.println(new String(buf.array(),0,len));
+            buf.clear();
+        }
+        inChannel.close();
+        sChannel.close();
+    }
+
+    //服务端
+    @Test
+    public void server() throws Exception{
+        ServerSocketChannel ssChannel=ServerSocketChannel.open();
+        FileChannel outChannel=FileChannel.open(Paths.get("2.jpg"), StandardOpenOption.WRITE,StandardOpenOption.CREATE);
+        ssChannel.bind(new InetSocketAddress(9898));
+        SocketChannel sChannel=ssChannel.accept();
+        ByteBuffer buf=ByteBuffer.allocate(1024);
+        while(sChannel.read(buf)!=-1){
+            buf.flip();
+            outChannel.write(buf);
+            buf.clear();
+        }
+
+        //发送反馈给客户端
+        buf.put("服务端接收数据成功".getBytes());
+        buf.flip();//给为读模式
+        sChannel.write(buf);
+
+        sChannel.close();
+        outChannel.close();
+        ssChannel.close();
     }
 }
