55af9d0dbae1236f9da68f254f2f3891004dad34
Ning-chuan <395378476@qq.com>
2020-10-15 23:13:23 +0800

    整合redis spring-test

--- Seckill/src/test/java/TestSpring.java
+++ Seckill/src/test/java/TestSpring.java
@@ -1,18 +1,28 @@
+import com.yuziyan.seckill.entity.User;
+import com.yuziyan.seckill.service.UserService;
+import com.yuziyan.seckill.service.impl.UserServiceImpl;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.context.support.XmlWebApplicationContext;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-*.xml"})
 public class TestSpring {
+    
+    @Autowired
+    UserService userService;
+    
     /**
-     * 用于测试：
+     * 用于测试：spring-test使用
      */
     @Test
-    public void test1(){
-////        XmlWebApplicationContext ctx = new XmlWebApplicationContext();
-//        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext();
-//        String[] names = ctx.getBeanDefinitionNames();
-//        for (String name : names) {
-//            System.out.println("name = " + name);
-//        }
+    public void test(){
+        User user = userService.getUser(1);
+        System.out.println("user = " + user);
     }
+    
 }
