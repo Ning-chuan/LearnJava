dedb5b387fb4b61924e67f7c7def020d766a6f0d
Ning-chuan <395378476@qq.com>
2021-02-19 19:06:19 +0800

    SpringBoot后台管理系统

---
+++ boot-web-admin/src/test/java/com/yuziyan/admin/BootWebAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuziyan.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootWebAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
